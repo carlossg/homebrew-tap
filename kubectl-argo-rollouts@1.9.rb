523caecba4241cc3d95e451efa0ab3f00d9049b3
Carlos Sanchez <carlos@apache.org>
2026-09-18 17:24:44 +0200

    chore: bump to 1.9.1
    Signed-off-by: Carlos Sanchez <carlos@apache.org>

--- kubectl-argo-rollouts@1.9.rb
+++ kubectl-argo-rollouts@1.9.rb
@@ -3,28 +3,28 @@ class KubectlArgoRolloutsAT19 < Formula
     desc "Kubectl Argo Rollouts Plugin."
     homepage "https://argoproj.io"
     baseurl = "https://github.com/argoproj/argo-rollouts/releases/download"
-    version "v1.9.0"
+    version "v1.9.1"
 
     if OS.mac? && Hardware::CPU.arm?
       kernel = "darwin"
       arch = "arm64"
-      sha256 "e822127ff7a783739c23b0f9e3005910f4cb7cf5b52a281cbf41c409fa3c9e80"
+      sha256 "e2ada94223e4cfffd8fbc14724e230b072becd007a7fde6f3296a95bcf56596e"
     elsif OS.mac? && Hardware::CPU.intel?
       kernel = "darwin"
       arch = "amd64"
-      sha256 "76b557ca363d5c5ac5d9bffff35e93e9bca5106537aef1cf7a927d67ff8b86fe"
+      sha256 "1212551121b2d41ea61ea1f11703e486c34915145d3894c718e613d31e472305"
     elsif OS.linux? && Hardware::CPU.arm?
       kernel = "linux"
       arch = "arm64"
-      sha256 "90907a84406b3843fb9deecd12a7312f61a9269e6d5729a30ca72a26bf67f2ab"
+      sha256 "44f4f5dc0668430cadc5f4cc52e4ae1bf0a3f2f60320af6b3b2edd3c826402b5"
     elsif OS.linux? && Hardware::CPU.intel?
       kernel = "linux"
       arch = "amd64"
-      sha256 "d1c98f59f6d3716b7a35aa540506764700a358448dd6a10e92381b7aa294d00b"
+      sha256 "466d93ccf767e975691359c6c9bb6750696e54c336d28ccd4d39e0f0519e8bf6"
     end
 
     @@bin_name = "kubectl-argo-rollouts-" + kernel + "-" + arch
-    url baseurl + "/v1.9.0/" + @@bin_name
+    url baseurl + "/v1.9.1/" + @@bin_name
 
     def install
       bin.install @@bin_name
